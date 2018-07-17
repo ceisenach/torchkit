+ only make new jacobian if wrong shape

autodiff/core.py
@@ -37,8 +37,10 @@ class JParameter(nn.Parameter):
         # check if its already been updated at least once
         if not self.__jacobian_ready:
             shape = D.shape if mode=='batch' else D.shape[1:] if mode=='sum' else None
-            self._jacobian = torch.zeros(shape)
-
+            if not (self._jacobian is not None and self._jacobian.shape == shape):
+                # print('Mode: %s. D shape: %s. J shape: %s' %(mode,str(D.shape),str(shape)))
+                self._jacobian = torch.zeros(shape)
+                
         # update
         if mode == 'batch':
             pass