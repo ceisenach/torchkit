6fbe3cf9d391b13e4ead9546e636fe3fb4feebbc
Carson Eisenach <carson@eisenach.org>
2018-07-17 22:48:32 -0400

    + can run trpo training with new framework wrapped

--- model.py
+++ model.py
@@ -16,45 +16,19 @@ class Policy(nn.Module):
         self.action_mean.weight.data.mul_(0.1)
         self.action_mean.bias.data.mul_(0.0)
 
-        self.action_log_std = nn.Parameter(torch.zeros(num_outputs))
-
-    def forward(self, x):
-        x = F.tanh(self.affine1(x))
-        x = F.tanh(self.affine2(x))
-
-        action_mean = self.action_mean(x)
-        action_log_std = self.action_log_std.expand_as(action_mean)
-        action_std = torch.exp(action_log_std)
-
-        return action_mean, action_log_std, action_std
-
-
-
-class Policy2(nn.Module):
-    def __init__(self, num_inputs, num_outputs):
-        super(Policy2, self).__init__()
-        self.affine1 = nn.Linear(num_inputs, 10)
-
-        self.action_mean = nn.Linear(10, num_outputs)
-        self.action_mean.weight.data.mul_(0.1)
-        self.action_mean.bias.data.mul_(0.0)
-
-        self.action_log_std = nn.Parameter(data= 1. + 0.1*torch.randn(num_outputs))
+        self.action_log_std = nn.Parameter(data=torch.zeros(num_outputs))
 
 
     def forward(self, x,save_for_jacobian=False,**kwargs):
         x = self.affine1(x,save_for_jacobian)
         x = F.tanh(x,save_for_jacobian)
+        x = self.affine2(x,save_for_jacobian)
+        x = F.tanh(x,save_for_jacobian)
         action_mean = self.action_mean(x,save_for_jacobian)
         action_log_std = self.action_log_std.expand_as(action_mean.data)
 
         return action_mean, action_log_std
-
-    def parameters(self,mean_only=False):
-        if mean_only:
-            return list(self.action_mean.parameters()) + list(self.affine1.parameters())
-        else:
-            return super(Policy2,self).parameters()
+        
 
 
 class Value(nn.Module):
